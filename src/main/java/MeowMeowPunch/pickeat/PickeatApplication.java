c154c5903933f988f6e5c39f9698880160f12e3b
junoyou <ryuwon_@naver.com>
2025-12-03 22:12:33 +0900

    Init: 초기파일 세팅

---
+++ src/main/java/MeowMeowPunch/pickeat/PickeatApplication.java
@@ -0,0 +1,13 @@
+package MeowMeowPunch.pickeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MeowMeowPunch/pickeat/PickeatApplicationTests.java
@@ -0,0 +1,13 @@
+package MeowMeowPunch.pickeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
