03d22626cbf614bddda778bc18a81626d5194b5b
junoyou <ryuwon_@naver.com>
2025-12-10 15:13:00 +0900

    ♻️ Refactor[#16]: 정적 팩토리 메서드(success, error) 도입 및 data 필드 Null 제외 처리

--- src/main/java/MeowMeowPunch/pickeat/global/common/template/ResTemplate.java
+++ src/main/java/MeowMeowPunch/pickeat/global/common/template/ResTemplate.java
@@ -1,5 +1,6 @@
 package MeowMeowPunch.pickeat.global.common.template;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -7,22 +8,28 @@ import org.springframework.http.HttpStatus;
 
 @Slf4j
 @Getter
-@JsonPropertyOrder({"code","message","data"})
+@JsonPropertyOrder({ "code", "message", "data" })
 public class ResTemplate<T> {
 	int code;
 	String message;
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	T data;
 
-	// 성공하였을 경우
-	public ResTemplate(HttpStatus httpStatus, String message, T data) {
+	// Private 생성자 => success, error 메서드로 반환
+	private ResTemplate(HttpStatus httpStatus, String message, T data) {
 		this.code = httpStatus.value();
 		this.message = message;
 		this.data = data;
 	}
 
-	// 실패하였을 경우
-	public ResTemplate(HttpStatus httpStatus, String message) {
-		this.code = httpStatus.value();
-		this.message = message;
+	// 성공
+	public static <T> ResTemplate<T> success(HttpStatus httpStatus, String message, T data) {
+		return new ResTemplate<>(httpStatus, message, data);
+	}
+
+	// 실패
+	public static <T> ResTemplate<T> error(HttpStatus httpStatus, String message) {
+		return new ResTemplate<>(httpStatus, message, null);
 	}
 }
