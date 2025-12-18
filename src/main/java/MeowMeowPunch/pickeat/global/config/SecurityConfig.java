7564848d3bfa5411cdbe426336b232cb8467ade6
Junho Yu <ryuwon@gnu.ac.kr>
2025-12-18 15:58:36 +0900

    🐛 Fix: 충돌 해결

--- src/main/java/MeowMeowPunch/pickeat/global/config/SecurityConfig.java
+++ src/main/java/MeowMeowPunch/pickeat/global/config/SecurityConfig.java
@@ -6,11 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import MeowMeowPunch.pickeat.global.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,12 +31,23 @@ public class SecurityConfig {
 			)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(
-					"/diet/**",     // 식단 API
+					"/auth/login",
+					"/auth/regist",
+					"/diet/**",      // 주윤
 					"/internal/**"
 				).permitAll()
-				.anyRequest().authenticated()   // 나머지는 나중에 JWT 붙일 때 사용
-			);
+				.anyRequest().authenticated()
+			)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+
+	/**
+	 * [Security][Bean] 패스워드 인코더 (OAuth 기반이지만 추후 비밀번호 인증 대비)
+	 */
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
