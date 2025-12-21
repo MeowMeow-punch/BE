4ee68388ac862c08bfe2437d619dab2194aa9f34
Junho Yu <ryuwon@gnu.ac.kr>
2025-12-21 14:06:39 +0900

    ♻️ Refactor[#36]: Repository 어노테이션 및 주석 추가

--- src/main/java/MeowMeowPunch/pickeat/domain/diet/repository/RecommendedDietFoodRepository.java
+++ src/main/java/MeowMeowPunch/pickeat/domain/diet/repository/RecommendedDietFoodRepository.java
@@ -5,7 +5,19 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import MeowMeowPunch.pickeat.domain.diet.entity.RecommendedDietFood;
+import org.springframework.stereotype.Repository;
 
+/**
+ * [Diet][Repository] 추천 식단-음식 매핑 정보 Repository.
+ */
+@Repository
 public interface RecommendedDietFoodRepository extends JpaRepository<RecommendedDietFood, Long> {
+
+	/**
+	 * 특정 추천 식단에 포함된 음식 목록 조회.
+	 *
+	 * @param recommendedDietId 추천 식단 ID
+	 * @return 추천 식단-음식 매핑 리스트
+	 */
 	List<RecommendedDietFood> findAllByRecommendedDietId(Long recommendedDietId);
 }
