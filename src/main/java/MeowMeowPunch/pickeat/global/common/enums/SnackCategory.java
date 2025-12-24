9ad17e359225a912dae1b041571a09dd6e9c8923
Jang4360 <jooyoon4360@gmail.com>
2025-12-24 23:23:32 +0900

    REFACTOR[#44] 카테고리명 수정

--- src/main/java/MeowMeowPunch/pickeat/global/common/enums/SnackCategory.java
+++ src/main/java/MeowMeowPunch/pickeat/global/common/enums/SnackCategory.java
@@ -5,10 +5,10 @@ import java.util.stream.Stream;
 
 // 간식 추천에서 허용하는 카테고리 집합
 public enum SnackCategory {
-	BAKERY("빵 및 과자류"),
-	DAIRY("유제품류 및 빙과류"),
-	BEVERAGE("음료 및 차류"),
-	FRUIT("과일류");
+	BAKERY("빵 및 과자"),
+	DAIRY("유제품류 및 빙과"),
+	BEVERAGE("음료 및 차"),
+	FRUIT("과일");
 
 	private final String label;
 
